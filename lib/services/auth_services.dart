b214f331e5adf01169166d4a6634f558f209a100
nihalninu25 <nihalninu25@gmail.com>
2022-03-09 05:38:59 +0530

    second commit

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -2,11 +2,19 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthServices {
   Future signIn(email, passwd) async {
-    await FirebaseAuth.instance
-        .signInWithEmailAndPassword(email: email, password: passwd);
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: passwd);
+    } catch (e) {
+      print(e);
+    }
   }
 
   Future signOut() async {
-    await FirebaseAuth.instance.signOut();
+    try {
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      print(e);
+    }
   }
 }
